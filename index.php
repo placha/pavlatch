d5eab0697d3fd8fc039e12a4bca41b67906973d6
Kacper Placha <kacper@placha.pl>
2020-06-07 01:57:46 +0200

    Add config

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 include __DIR__ . '/vendor/autoload.php';
 
-new \pavlatch\Server(['secureKey' => '6a4068f2-2cde-494d-90e1-08ba5827a677']);
+$config = include __DIR__ . '/config.php';
+
+if ($config['env'] === 'dev') {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+}
+
+new \pavlatch\Server($config);
